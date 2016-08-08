d0654f1ae7fc7e1fe15aacb799f665a0451f3458
Ian Wilcox and Vichitra Pool <ian+vichitra@>
2016-08-08 16:10:06 -0700

    capitalization

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,10 @@
+$(function() {
+
   var sentence = prompt("Enter a sentence");
-  alert(sentence.substr(1,sentence.length-2));
-
-
-/*
   var f = sentence.charAt(0);
   var l = sentence.charAt(sentence.length-1);
   var letterF = f.toUpperCase();
   var letterL = l.toUpperCase();
-  var newsentence = sentence.replace(/f/i,letterF);
-  var newsentence = sentence.replace(/l/i,letterL);
+  var newsentence = letterF + sentence.substr(1,sentence.length-2) + letterL ;
   alert(newsentence);
-dd*
-
-
-// var sentence = prompt("Enter a sentence")
-// var letterF = sentence.charAt(0).toUpperCase()
-// var letterL = sentence.charAt(sentence .length-1).toUpperCase()
-// alert(sentence)
-
-// alert(letterF)
-// alert(letterL)
-//
-// var letterF = letterF.toUpperCase()
-// var letterL =letterL.toUpperCase()
-//
-// alert(letterF)
-// alert(letterL)
-
 });
