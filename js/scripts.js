f9b26b07247df0e6afd9ee2a89ae7b3d48329850
Ian Wilcox and Vichitra Pool <ian+vichitra@>
2016-08-08 16:01:52 -0700

    remove first & last letter

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+  var sentence = prompt("Enter a sentence");
+  alert(sentence.substr(1,sentence.length-2));
+
+
+/*
+  var f = sentence.charAt(0);
+  var l = sentence.charAt(sentence.length-1);
+  var letterF = f.toUpperCase();
+  var letterL = l.toUpperCase();
+  var newsentence = sentence.replace(/f/i,letterF);
+  var newsentence = sentence.replace(/l/i,letterL);
+  alert(newsentence);
+dd*
+
+
+// var sentence = prompt("Enter a sentence")
+// var letterF = sentence.charAt(0).toUpperCase()
+// var letterL = sentence.charAt(sentence .length-1).toUpperCase()
+// alert(sentence)
+
+// alert(letterF)
+// alert(letterL)
+//
+// var letterF = letterF.toUpperCase()
+// var letterL =letterL.toUpperCase()
+//
+// alert(letterF)
+// alert(letterL)
+
+});
