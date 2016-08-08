92866ad07111f7817cbfff6d5e88a875e4d97a3c
Ian Wilcox and Vichitra Pool <ian+vichitra@>
2016-08-08 16:30:29 -0700

    add center letter to the front

--- js/scripts.js
+++ js/scripts.js
@@ -11,5 +11,11 @@ $(function() {
   var newsentence = letterL + sentence.substr(1,sentence.length-2) + letterF ;
   alert(newsentence);
 
+  var newsentence = sentence.substr(0,sentence.length-1) + letterL + letterF ;
+  alert(newsentence);
 
+  var length = sentence.charAt((sentence.length)/2)
+  var length = length.toUpperCase();
+  var newsentence = length + sentence + letterL + letterF ;
+  alert(newsentence);
 });
